bacea07964ad819b33d59a881a5a2851382fa397
Kchanit <chanitaphon.l@ku.th>
2024-01-04 08:19:34 +0700

    fix: price display

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product;
+use App\Models\Stock;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Validator;
@@ -12,12 +13,21 @@ class ProductController extends Controller
 {
     private function formatProduct($product)
     {
+        $stocks = $product->stocks;
+        $lowestPrice = PHP_INT_MAX; // Initialize with the maximum possible integer value
+
+        foreach ($stocks as $stock) {
+            if ($stock->price < $lowestPrice) {
+                $lowestPrice = $stock->price;
+            }
+        }
+
         return [
             'id' => $product->id,
             'name' => $product->name,
             'description' => $product->description,
             'category' => $product->category->name,
-            'price' => $product->price,
+            'price' => $lowestPrice,
             'status' => $product->status,
             'gender' => $product->gender,
             'images' => self::formatImages($product->images),
@@ -79,9 +89,7 @@ class ProductController extends Controller
         $validator = Validator::make($request->all(), [
             'name' => 'required|string',
             'description' => 'required|string',
-            'price' => 'required|numeric',
             'category_id' => 'required|exists:categories,id',
-            'categories_id' => 'required|exists:categories,id',
             'gender' => 'required|string',
         ]);
 
@@ -92,15 +100,38 @@ class ProductController extends Controller
         $product = new Product([
             'name' => $request->input('name'),
             'description' => $request->input('description'),
-            'price' => $request->input('price'),
             'category_id' => $request->input('category_id'),
             'gender' => $request->input('gender'),
         ]);
         $product->save();
-        $product->categories()->sync($request->input('categories_id'));
-
-        // Store product data in Redis
+        $product->category()->associate($request->input('category_id'));
+        // $product->categories()->sync($request->input('categories_id'));
+        // Store all product data in Redis
         Redis::set('products', json_encode(Product::all()));
+
+        // Create stocks process
+        $stocks = $request->input('stocks');
+        foreach ($stocks as $stockData) {
+            $color_id = $stockData['color_id'];
+
+            foreach ($stockData['sizes'] as $sizeData) {
+                $size_id = $sizeData['size_id'];
+                $quantity = $sizeData['quantity'];
+                $price = $sizeData['price'];
+                // Create stock
+                Stock::create(
+                    [
+                        'product_id' => $product->id,
+                        'color_id' => $color_id,
+                        'size_id' => $size_id,
+                        'quantity' => $quantity,
+                        'price' => $price
+                    ],
+                );
+            }
+        }
+        // Store all stocks in Redis
+        Redis::set('stocks', json_encode(Stock::all()));
 
         return response()->json(['message' => 'Product created successfully', 'product' => $product], 201);
     }
