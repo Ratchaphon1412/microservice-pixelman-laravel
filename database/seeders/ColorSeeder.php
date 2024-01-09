d6f4e7dec1a89a90a826e93404f41bdf876326f1
ratchaphon1412 <ratchaphon.h111@gmail.com>
2024-01-09 18:52:24 +0700

    edit ColorSeeder

--- database/seeders/ColorSeeder.php
+++ database/seeders/ColorSeeder.php
@@ -2,9 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\Color;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Color;
 
 class ColorSeeder extends Seeder
 {
@@ -13,20 +13,7 @@ class ColorSeeder extends Seeder
      */
     public function run(): void
     {
-        $color = new Color();
-        $color->hex_color = '#ffffff';
-        $color->save();
-
-        $color = new Color();
-        $color->hex_color = '#000000';
-        $color->save();
-
-        $color = new Color();
-        $color->hex_color = '#ff0000';
-        $color->save();
-
-        $color = new Color();
-        $color->hex_color = '#00ff00';
-        $color->save();
+        //
+        Color::factory()->count(10)->create();
     }
 }
